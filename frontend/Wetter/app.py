1a5279cb0ffbf78147c4f8fd7fb2b7cdc107441c
Reeeees <theresawagner1@gmx.net>
2026-03-09 13:36:47 +0100

    Update app.py

--- frontend/Wetter/app.py
+++ frontend/Wetter/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/"), methods=["GET","POST"])
 def home():
+	if request.method == "POST":
+		standort = request.form["standort"]
+		datum = request.form["datum"]
+
+		print("Standort:", standort)
+		print("Datum:", datum)
+
 	return render_template("index.html")
 
 if __name__ == "__main__":
